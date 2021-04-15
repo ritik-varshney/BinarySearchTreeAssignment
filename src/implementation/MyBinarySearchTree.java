52d5e4f8ce281d8cdbae1b86f546a5e5decc597a
ritik varshney <ritik.varshney_cs19@gla.ac.in>
2021-04-15 22:54:46 +0530

    create insert method

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -1,7 +1,6 @@
 package implementation;
 
 import myinterface.BinarySearchTreeADT;
-import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
@@ -13,11 +12,45 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
 
     @Override
     public void insert(E data) {
+        java.lang.Object element;
+        Node<E> node = new Node<>(element);
+        if (isEmpty()) {
+            root = node;
+        } else {
+            //traverse and reach the position where new node
+            // will be inserted as well as keep reference of parent
+            Node<E> temp = root;
+            Node<E> parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (element.compareTo(temp.getData()) <= 0) {
+                    //update temp to refer left subtree
+                    temp = temp.getLeft();
+                } else {
+                    //update temp to refer right subtree
+                    temp = temp.getRight();
+                }
+            }
+            //checking whether new node will be left or right
+            //child of parent
+            if (element.compareTo(parent.getData()) <= 0) {
+                //set new node to left child of parent
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
 
+    }
+
+    private boolean isEmpty() {
+        return root==null;
     }
 
     @Override
     public boolean  search(E searchElement) {
+
+
         return false;
     }
 
